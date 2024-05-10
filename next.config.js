9deee991644f6d6d55d615fdf93fbf28a267d84f
Tyler Candee <tyler@candeegenerations.com>
2024-05-10 11:47:03 -0400

    chore: build fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+/* eslint-disable no-undef */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    workerThreads: false,
+    cpus: 1,
+  },
 }
 
 module.exports = nextConfig
